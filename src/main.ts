882c6520ac1b9ac664878978b00d875169234504
camdiaz <cdiaz19005@gmail.com>
2026-09-15 19:28:23 -0500

    feat: characters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './module/app.module';
+import { CharactersModule } from './character/character.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(CharactersModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
